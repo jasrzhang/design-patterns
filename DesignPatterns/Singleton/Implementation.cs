1fb741208571b515d34927f664d5b8e02fafabd1
jasrzhang <jasrzhang@gmail.com>
2024-08-05 12:37:46 +0930

    Singleton method

---
+++ DesignPatterns/Singleton/Implementation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        // This could be not thread safe, use Lazy<T> can make sure thread safe
+        // private static Logger? _instance;
+
+        // Lazy<T>
+        private static readonly Lazy<Logger> _lazyLogger = new Lazy<Logger>(() => new Logger());
+
+        public static Logger Instance
+        {
+            get
+            {
+                return _lazyLogger.Value;
+                //if (_instance == null)
+                //{
+                //    _instance = new Logger();
+                //}
+                //return _instance;
+            }
+        }
+
+        public Logger()
+        {
+            
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
+}
---
+++ DesignPatterns/Singleton/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Singleton;
+
+Console.Title = "Singleton pattern";
+
+// call the property getter twice
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if(instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Instaces are the same");
+}
+
+instance1.Log($"Message from {nameof(instance1)}");
+instance1.Log($"Message from {nameof(instance2)}");
+
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+Console.ReadLine();
